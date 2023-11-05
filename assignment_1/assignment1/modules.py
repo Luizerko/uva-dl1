950deca4099b26fa2b5fe2c78f081e4b5c1c6ba8
Lui <luisvz@gmail.com>
2023-11-05 20:32:33 +0100

    Modules with numpy implemented

---
+++ assignment_1/assignment1/modules.py
@@ -0,0 +1,357 @@
+################################################################################
+# MIT License
+#
+# Copyright (c) 2023 University of Amsterdam
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to conditions.
+#
+# Author: Deep Learning Course (UvA) | Fall 2023
+# Date Created: 2023-11-01
+################################################################################
+"""
+This module implements various modules of the network.
+You should fill in code into indicated sections.
+"""
+import numpy as np
+
+
+class LinearModule(object):
+    """
+    Linear module. Applies a linear transformation to the input data.
+    """
+
+    def __init__(self, in_features, out_features, input_layer=False):
+        """
+        Initializes the parameters of the module.
+
+        Args:
+          in_features: size of each input sample
+          out_features: size of each output sample
+          input_layer: boolean, True if this is the first layer after the input, else False.
+
+        TODO:
+        Initialize weight parameters using Kaiming initialization. 
+        Initialize biases with zeros.
+        Hint: the input_layer argument might be needed for the initialization
+
+        Also, initialize gradients with zeros.
+        """
+
+        # Note: For the sake of this assignment, please store the parameters
+        # and gradients in this format, otherwise some unit tests might fail.
+        self.params = {'weight': None, 'bias': None} # Model parameters
+        self.grads = {'weight': None, 'bias': None} # Gradients
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        if input_layer:
+          self.params['weight'] = np.identity(in_features)
+        else:
+          self.params['weight'] = np.random.normal(scale=np.sqrt(2/in_features), size=(out_features, in_features))
+          
+        self.params['bias'] = np.zeros((1, out_features))
+
+        self.grads['weight'] = np.zeros((out_features, in_features))
+        self.grads['bias'] = np.zeros((1, out_features))
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+    def forward(self, x):
+        """
+        Forward pass.
+
+        Args:
+          x: input to the module
+        Returns:
+          out: output of the module
+
+        TODO:
+        Implement forward pass of the module.
+
+        Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        self.input = x
+
+        out = x @ self.params['weight'].transpose() + np.vstack([self.params['bias'] for n in range(x.shape[0])])
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, dout):
+        """
+        Backward pass.
+
+        Args:
+          dout: gradients of the previous module
+        Returns:
+          dx: gradients with respect to the input of the module
+
+        TODO:
+        Implement backward pass of the module. Store gradient of the loss with respect to
+        layer parameters in self.grads['weight'] and self.grads['bias'].
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        dx = dout @ self.params['weight']
+        
+        self.grads['weight'] = dout.transpose()@self.input
+        self.grads['bias'] = np.ones((1, self.input.shape[0]))@dout
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+        return dx
+
+    def clear_cache(self):
+        """
+        Remove any saved tensors for the backward pass.
+        Used to clean-up model from any remaining input data when we want to save it.
+
+        TODO:
+        Set any caches you have to None.
+        """
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        self.input = None
+        #self.grads = {'weight': None, 'bias': None}
+        
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+
+class ELUModule(object):
+    """
+    ELU activation module.
+    """
+
+    def forward(self, x):
+        """
+        Forward pass.
+
+        Args:
+          x: input to the module
+        Returns:
+          out: output of the module
+
+        TODO:
+        Implement forward pass of the module.
+        
+        Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        """
+        
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        self.input = x
+
+        out = np.where(x > 0, x, np.exp(x) - 1)
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, dout):
+        """
+        Backward pass.
+        Args:
+          dout: gradients of the previous module
+        Returns:
+          dx: gradients with respect to the input of the module
+
+        TODO:
+        Implement backward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        dx = dout * np.where(self.input > 0, 1, np.exp(self.input))
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+        return dx
+
+    def clear_cache(self):
+        """
+        Remove any saved tensors for the backward pass.
+        Used to clean-up model from any remaining input data when we want to save it.
+
+        TODO:
+        Set any caches you have to None.
+        """
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        
+        self.input = None
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+
+class SoftMaxModule(object):
+    """
+    Softmax activation module.
+    """
+
+    def forward(self, x):
+        """
+        Forward pass.
+        Args:
+          x: input to the module
+        Returns:
+          out: output of the module
+
+        TODO:
+        Implement forward pass of the module.
+        To stabilize computation you should use the so-called Max Trick - https://timvieira.github.io/blog/post/2014/02/11/exp-normalize-trick/
+
+        Hint: You can store intermediate variables inside the object. They can be used in backward pass computation.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        const_aux = np.tile(np.max(x, axis=1)[:, np.newaxis], (1, x.shape[1]))
+        exp_aux = np.exp(x - const_aux)
+        sum_aux = np.tile(np.sum(exp_aux, axis=1)[:, np.newaxis], (1, x.shape[1]))
+        out = exp_aux/sum_aux
+
+        self.output = out
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, dout):
+        """
+        Backward pass.
+        Args:
+          dout: gradients of the previous modul
+        Returns:
+          dx: gradients with respect to the input of the module
+
+        TODO:
+        Implement backward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        dx = self.output * (dout - (dout * self.output)@np.ones((dout.shape[1], dout.shape[1])))
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return dx
+
+    def clear_cache(self):
+        """
+        Remove any saved tensors for the backward pass.
+        Used to clean-up model from any remaining input data when we want to save it.
+
+        TODO:
+        Set any caches you have to None.
+        """
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        
+        self.output = None
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+
+class CrossEntropyModule(object):
+    """
+    Cross entropy loss module.
+    """
+
+    def forward(self, x, y):
+        """
+        Forward pass.
+        Args:
+          x: input to the module
+          y: labels of the input
+        Returns:
+          out: cross entropy loss
+
+        TODO:
+        Implement forward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+
+        t = np.zeros(x.shape)
+        t[np.arange(y.size), y] = 1
+        out = np.mean(-np.sum(t*np.log(x), axis=1))
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return out
+
+    def backward(self, x, y):
+        """
+        Backward pass.
+        Args:
+          x: input to the module
+          y: labels of the input
+        Returns:
+          dx: gradient of the loss with the respect to the input x.
+
+        TODO:
+        Implement backward pass of the module.
+        """
+
+        #######################
+        # PUT YOUR CODE HERE  #
+        #######################
+        t = np.zeros(x.shape)
+        t[np.arange(y.size), y] = 1
+        dx = (-1/x.shape[0])*t/x
+
+        #######################
+        # END OF YOUR CODE    #
+        #######################
+
+        return dx
